74c9d00f4d9f8db2e8032ec11c9fbe6e40600951
lordSosa15 <97013669+lordSosa15@users.noreply.github.com>
2022-10-03 23:11:19 -0400

    form update

--- src/pages/CreateForm.jsx
+++ src/pages/CreateForm.jsx
@@ -5,34 +5,41 @@ import { Link } from "react-router-dom";
 const CreateForm = () => {
   return (
     <div>
+    <br />
+    <h1>Enter event info here</h1>
+    <br />
       <form>
         <InputLabel>
-          {" "}
           Event name:
+          <br />
           <Input type="text" />
         </InputLabel>
 
         <br />
         <InputLabel>
-          {" "}
-          Start and End
+          Start <br />
           <Input type="date" />
         </InputLabel>
         <br />
         <InputLabel>
-          {" "}
+        End: <br />
+          <Input type="date"/>
+        </InputLabel>
+        <br />
+        <InputLabel>
           Host name:
+          <br />
           <Input type="text" />
         </InputLabel>
         <br />
         <InputLabel>
-          Location
-          <Input> </Input>
+          Location <br />
+          <Input type="text"> </Input>
         </InputLabel>
         <br />
-        <InputLabel>
-          Event photo
-          <Input type="file"></Input>
+        <InputLabel htmlFor="fileInput" >
+          Event photo <br />
+          <Input type="file" ></Input>
         </InputLabel>
         <Link to="/Event">
           <button className="custom__button">NEXT</button>
--- src/pages/Event.jsx
+++ src/pages/Event.jsx
@@ -8,20 +8,37 @@ const Event = () => {
   return (
     <div className="App__event-content">
       <img src={images.event} alt="" />
-      <h3>Birthday Bash</h3>
-      <p>Hosted by Elysia</p>
-      <button className="custom__button"> 🤝 Invite</button>
-      <div>
-        <img src={images.calendar} alt="" />
+      <div className="app__wrapper_info">
+        <h1>Birthday Bash</h1>
+        <p>
+          Hosted by <span style={{ fontWeight: "bold" }}> Elysia </span>
+          {/* <button className="custom__button"> 🤝 Invite</button> */}
+        </p>
+        {/* <button className="custom__button"> 🤝 Invite</button> */}
+        <div>
+          <img
+            src={images.calendar}
+            alt="calendar icon"
+            className="app__event-icon"
+          />
+        </div>
+        <h1 style={{ fontSize: "16px", lineHeight: "18px" }}>
+          18 August 6:00PM
+        </h1>
+        <h2 style={{ fontSize:"14px" }}>
+          to <span style={{ fontWeight: "bold" }}>19 August 1:00PM</span> UTC
+          +10
+        </h2>{" "}
+        <IoIosArrowForward />
+        <br />
+        <div>
+          <img src={images.locationIcon} alt="" className="app__event-icon" />
+        </div>
+        <h1 style={{ fontSize: "16px", lineHeight: "18px", flex: "none" }}>
+          Street name
+        </h1>
+        <h2 style={{ fontSize:"14px" }}>Suburb, State, Postcode</h2> <IoIosArrowForward />
       </div>
-      <h3>18 August 6:00PM</h3>
-      <p>to 19 August 1:00PM UTC +10</p> <IoIosArrowForward />
-      <br />
-      <div>
-        <img src={images.locationIcon} alt="" />
-      </div>
-      <h3>Street name</h3>
-      <p>Suburb, State, Postcode</p> <IoIosArrowForward />
       <UrlBar />
     </div>
   );
